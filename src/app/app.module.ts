89c7576d7375ffad3eee351e485f6bdf8aa0c76a
andreaibt21 <andreaixabella@gmail.com>
2023-09-07 17:56:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ErrorComponent,
+    BienvenidoComponent
   ],
   imports: [
     BrowserModule
